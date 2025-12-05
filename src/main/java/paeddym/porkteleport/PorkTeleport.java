0a8f9fc060fa86932875666d7193ed0b0dd4dec5
Patrik Marosváry <patrikmarosvary@gmail.com>
2025-12-06 00:15:18 +0100

    fix: Removed useless code, changed dependencies

--- src/main/java/paeddym/porkteleport/PorkTeleport.java
+++ src/main/java/paeddym/porkteleport/PorkTeleport.java
@@ -1,24 +1,44 @@
 package paeddym.porkteleport;
-
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.server.world.ServerWorld;
+import java.util.Set;
+import net.minecraft.entity.Entity.RemovalReason;
+ 
 public class PorkTeleport implements ModInitializer {
-	public static final String MOD_ID = "porkteleport";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    public static final String MOD_ID = "porkteleport";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        UseItemCallback.EVENT.register((player, world, hand) -> {
+            ItemStack stack = player.getStackInHand(hand);
+            if (!world.isClient && (stack.isOf(Items.PORKCHOP) || stack.isOf(Items.COOKED_PORKCHOP))) {
+                if (player instanceof ServerPlayerEntity serverPlayer) {
+                    ServerWorld destWorld = serverPlayer.getServer().getWorld(World.NETHER);
+                    if (destWorld != null) {
+                        serverPlayer.teleport(
+                            destWorld,
+                            destWorld.getSpawnPos().getX() + 0.5,
+                            destWorld.getSpawnPos().getY(),
+                            destWorld.getSpawnPos().getZ() + 0.5,
+                            Set.of(),
+                            serverPlayer.getYaw(),
+                            serverPlayer.getPitch(),
+                            false
+                        );
+                    }
+                }
+            }
+            return ActionResult.SUCCESS;
+        });
+    }
 }
